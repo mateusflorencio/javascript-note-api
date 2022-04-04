8a39312804102e72bab48a45c92f63b5e6bf1658
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-04 18:19:37 -0300

    pegando a nota por id

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Note = require("../model/note");
-const withAuth = require("../middlewares/auth")
+const WithAuth = require("../middlewares/auth")
 
-router.post("/", withAuth, async (req, res) => {
+router.post("/", WithAuth, async (req, res) => {
     const {
         title,
         body
@@ -24,5 +24,34 @@ router.post("/", withAuth, async (req, res) => {
         });
     }
 });
+
+router.get("/:id", WithAuth, async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params;
+        let note = await Note.findById(id)
+        if (isOwner(req.user, note)) {
+            res.json(note);
+        } else {
+            res.status(403).json({
+                error: "Permission denied"
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: "Problem to get a note"
+        });
+    }
+});
+
+const isOwner = (user, note) => {
+    if (JSON.stringify(user._id) == JSON.stringify(note.author._id))
+        return true;
+    else {
+        return false;
+    };
+};
+
 
 module.exports = router;
