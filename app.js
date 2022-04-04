f0df43a9e3eab6b3dc40f63ab1ae95aa5b90a0bd
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-04 19:56:03 -0300

    liberando o cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 require("./config/database")
+const cors = require("cors");
 
 const usersRouter = require('./app/routes/users');
 const noteRouter = require('./app/routes/notes');
@@ -13,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', noteRouter);
