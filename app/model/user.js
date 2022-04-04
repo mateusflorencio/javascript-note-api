0f2993cc2ff460cca90a08e3a7cd3c3c4b48e266
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-04 11:55:34 -0300

    endpoint registro de usuario

--- app/model/user.js
+++ app/model/user.js
@@ -37,4 +37,4 @@ userSchema.pre("save", function (next) {
         })
     }
 })
-module.exports = mongoose.Model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../model/user");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.json("oi");
+router.post('/register', async (req, res) => {
+  const {
+    name,
+    email,
+    password
+  } = req.body;
+  const user = new User({
+    name,
+    email,
+    password
+  });
+
+  try {
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      error: "registering new user"
+    });
+  }
 });
 
 module.exports = router;
