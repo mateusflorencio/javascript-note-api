093cd66911a72c36d8d113e8028d3898ff490dee
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-04 19:13:01 -0300

    endpoint delete notas criado

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -40,25 +40,25 @@ router.get("/:id", WithAuth, async (req, res) => {
             res.status(403).json({
                 error: "Permission denied"
             })
-        }
+        };
     } catch (error) {
         res.status(500).json({
             error: "Problem to get a note"
         });
-    }
+    };
 });
 
 router.get("/", WithAuth, async (req, res) => {
     try {
         let notes = await Note.find({
             author: req.user._id
-        })
+        });
         res.json(notes);
     } catch (error) {
         res.status(500).json({
             error: "Problem to get a note"
         });
-    }
+    };
 });
 
 
@@ -90,14 +90,40 @@ router.put("/:id", WithAuth, async (req, res) => {
             res.status(403).json({
                 error: "Permission denied"
             });
-        }
+        };
     } catch (error) {
         res.status(500).json({
             error: "Problem to update a note"
         });
-    }
+    };
 
 });
+
+
+router.delete("/:id", WithAuth, async (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    try {
+        const note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            await note.delete();
+            res.json({
+                message: "ok"
+            }).status(204);
+        } else {
+            res.status(403).json({
+                error: "Permission denied"
+            });
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            error: "Problem to delete a note"
+        });
+    }
+})
 
 const isOwner = (user, note) => {
     if (JSON.stringify(user._id) == JSON.stringify(note.author._id))
